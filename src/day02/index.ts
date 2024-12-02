c60126ba9470914ebbc6796189ec890f74285e79
elm-suf <elm.sufian@gmail.com>
2024-12-02 21:44:16 +0100

    tried another approach

--- src/day02/index.ts
+++ src/day02/index.ts
@@ -3,12 +3,18 @@ import run from "aocrunner";
 const parseInput = (rawInput: string): number[][] =>
   rawInput.split("\n").map((line) => line.split(" ").map(Number));
 
-function isValidReport(report: number[]): boolean {
-  const isDecreasing = report[0] > report[1];
+function isValidReport(report: number[], skipIndex = -1): boolean {
+  const isDecreasing =
+    report[0 === skipIndex ? 1 : 0] > report[1 === skipIndex ? 2 : 1];
 
-  for (let i = 1; i < report.length; i++) {
-    const difference = report[i] - report[i - 1];
-    const isConsistent = isDecreasing ? difference < 0 : difference > 0;
+  for (let i = 0; i < report.length - 1; i++) {
+    if (skipIndex === i) continue;
+
+    const currr = i === skipIndex ? i + 1 : i;
+    const next = i + 1 === skipIndex ? i + 2 : i + 1;
+
+    const difference = report[currr] - report[next];
+    const isConsistent = isDecreasing ? difference > 0 : difference < 0;
     const isWithinRange =
       Math.abs(difference) >= 1 && Math.abs(difference) <= 3;
 
@@ -22,17 +28,21 @@ function isValidReport(report: number[]): boolean {
 
 const part1 = (rawInput: string): number => {
   const reports = parseInput(rawInput);
-  return reports.filter(isValidReport).length;
+  return reports.filter((report) => isValidReport(report)).length;
 };
 
 function isValidReport2(report: number[]): boolean {
   if (isValidReport(report)) return true;
 
   for (let i = 0; i < report.length; i++) {
-    const newReport = [...report];
-    newReport.splice(i, 1);
-    if (isValidReport(newReport)) return true;
+    if (isValidReport(report, i)) return true;
   }
+
+  // for (let i = 0; i < report.length; i++) {
+  //   const newReport = [...report];
+  //   newReport.splice(i, 1);
+  //   if (isValidReport(newReport)) return true;
+  // }
 
   return false;
 }
@@ -62,12 +72,12 @@ run({
     tests: [
       {
         input: `
-7 6 4 2 1
-1 2 7 8 9
-9 7 6 2 1
-1 3 2 4 5
-8 6 4 4 1
-1 3 6 7 9`,
+  7 6 4 2 1
+  1 2 7 8 9
+  9 7 6 2 1
+  1 3 2 4 5
+  8 6 4 4 1
+  1 3 6 7 9`,
         expected: 4,
       },
     ],
