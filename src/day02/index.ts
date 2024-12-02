d7e7117996c7cc29ca5dc7280b5978d884ab01ff
elm-suf <elm.sufian@gmail.com>
2024-12-02 21:03:55 +0100

    day-2: part-2 brute force

--- src/day02/index.ts
+++ src/day02/index.ts
@@ -1,6 +1,5 @@
 import run from "aocrunner";
 
-
 const parseInput = (rawInput: string): number[][] =>
   rawInput.split("\n").map((line) => line.split(" ").map(Number));
 
@@ -10,7 +9,8 @@ function isValidReport(report: number[]): boolean {
   for (let i = 1; i < report.length; i++) {
     const difference = report[i] - report[i - 1];
     const isConsistent = isDecreasing ? difference < 0 : difference > 0;
-    const isWithinRange = Math.abs(difference) >= 1 && Math.abs(difference) <= 3;
+    const isWithinRange =
+      Math.abs(difference) >= 1 && Math.abs(difference) <= 3;
 
     if (!isConsistent || !isWithinRange) {
       return false;
@@ -25,10 +25,21 @@ const part1 = (rawInput: string): number => {
   return reports.filter(isValidReport).length;
 };
 
+function isValidReport2(report: number[]): boolean {
+  if (isValidReport(report)) return true;
+
+  for (let i = 0; i < report.length; i++) {
+    const newReport = [...report];
+    newReport.splice(i, 1);
+    if (isValidReport(newReport)) return true;
+  }
+
+  return false;
+}
 
 const part2 = (rawInput: string) => {
-  // const input = parseInput(rawInput);
-  return ;
+  const reports = parseInput(rawInput);
+  return reports.filter(isValidReport2).length;
 };
 
 run({
@@ -49,10 +60,16 @@ run({
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `
+7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9`,
+        expected: 4,
+      },
     ],
     solution: part2,
   },
