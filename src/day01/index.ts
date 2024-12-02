1a398eb1f2c8e9c8203cd1ad1ed756ef3c60d5d3
elm-suf <elm.sufian@gmail.com>
2024-12-02 20:04:38 +0100

    day-1: part-2

--- src/day01/index.ts
+++ src/day01/index.ts
@@ -1,19 +1,21 @@
 import run from "aocrunner";
 
-const part1 = (rawInput: string) => {
+const parseInput = (rawInput: string) => {
   const left: number[] = [];
   const right: number[] = [];
+  rawInput.split("\n").forEach((line) => {
+    const [l, r] = line.split("   ").map(Number);
+    left.push(l);
+    right.push(r);
+  });
+  return { right, left };
+};
 
-  rawInput
-    .split("\n")
-    .map((line) => line.split("   "))
-    .forEach(([l, r]) => {
-      right.push(+r);
-      left.push(+l);
-    });
+const part1 = (rawInput: string) => {
+  const { left, right } = parseInput(rawInput);
 
-  const sortedLeft = left.sort();
-  const sortedRight = right.sort();
+  const sortedLeft = left.sort((a, b) => a - b);
+  const sortedRight = right.sort((a, b) => a - b);
 
   const res = sortedLeft
     .map((el, i) => [el, sortedRight[i]])
@@ -24,25 +26,46 @@ const part1 = (rawInput: string) => {
 };
 
 const part2 = (rawInput: string) => {
-  return;
+  const { left, right } = parseInput(rawInput);
+
+  const count = right.reduce((map, el) => {
+    map.set(el, (map.get(el) || 0) + 1);
+    return map;
+  }, new Map<number, number>());
+
+  return left.reduce((sum, el) => sum + el * (count.get(el) ?? 0), 0);
 };
 
 run({
   part1: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `
+3   4
+4   3
+2   5
+1   3
+3   9
+3   3        
+        `,
+        expected: 11,
+      },
     ],
     solution: part1,
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `
+3   4
+4   3
+2   5
+1   3
+3   9
+3   3
+        `,
+        expected: 31,
+      },
     ],
     solution: part2,
   },
