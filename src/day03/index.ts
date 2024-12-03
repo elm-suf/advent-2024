41dd523a4d39d1246bb08c645d74e1946e4dd284
elm-suf <elm.sufian@gmail.com>
2024-12-03 19:06:00 +0100

    day-3: part-2 first solutio that comes to mind

--- src/day03/index.ts
+++ src/day03/index.ts
@@ -20,10 +20,63 @@ const part1 = (rawInput: string) => {
     .reduce((acc, curr) => (curr += acc), 0);
 };
 
-const part2 = (rawInput: string) => {
-  const input = parseInput(rawInput);
+// =============================================================== \\
+// ===================      part 2    ============================ \\
+// =============================================================== \\
 
-  return;
+function isValidPrefixToken(token: string, pattern: string) {
+  return pattern.startsWith(token);
+}
+
+const parseInput2 = (rawInput: string) => {
+  let newInput = "";
+  let startToken = "";
+  let endToken = "";
+  let ignore = false;
+  for (let index = 0; index < rawInput.length; index++) {
+    const element = rawInput[index];
+
+    if (ignore) {
+      endToken += element;
+    } else {
+      startToken += element;
+    }
+
+    if (endToken === `do()`) {
+      ignore = false;
+      endToken = "";
+      startToken = "";
+    } else if (!isValidPrefixToken(endToken, `do()`)) {
+      endToken = "";
+    }
+
+    if (startToken === `don't()`) {
+      ignore = true;
+      endToken = "";
+      startToken = "";
+    } else if (!isValidPrefixToken(startToken, `don't()`)) {
+      startToken = "";
+    }
+
+    if (!ignore) newInput += element;
+  }
+
+  // let index = rawInput.indexOf(`don't()`);
+  // let index1 = rawInput.indexOf(`do()`);
+  // do {
+  // console.log("index", index, index1);
+  // } while (index !== -1);
+
+  return parseInput(newInput);
+};
+
+const part2 = (rawInput: string) => {
+  const input = parseInput2(rawInput);
+
+  return input
+    .map((el) => el.substring(4, el.length - 1).split(","))
+    .map(([a, b]) => +a * +b)
+    .reduce((acc, curr) => (curr += acc), 0);
 };
 
 run({
@@ -38,13 +91,15 @@ run({
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))`,
+        expected: 48,
+      },
     ],
+
     solution: part2,
   },
+
   trimTestInputs: true,
   // onlyTests: true,
 });
